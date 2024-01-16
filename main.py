16566d5bfabc23d7d2d6b94bdc54b13632208bea
BenGornad <benjamin.gornadha@ext.adeo.com>
2024-01-16 21:19:49 +0100

    working

---
+++ main.py
@@ -0,0 +1,9 @@
+from tests.test_fizzbuzz import FizzBuzz
+
+if __name__ == '__main__':
+    input = [i for i in range(1, 101)]
+    fizz_buzz = FizzBuzz()
+    with open("result.txt", "w") as fd:
+        for number in input:
+            output = fizz_buzz.convert(number=number)
+            fd.writelines(f"{output}\n")
